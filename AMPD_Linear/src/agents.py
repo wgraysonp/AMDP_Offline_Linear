7b3ee0e658bf4cc2280ed929dd7cc465200aaf4a
William Powell <wgpowell@wisc.edu>
2025-05-21 22:33:22 -0500

    add get_problem_params function

--- AMPD_Linear/src/agents.py
+++ AMPD_Linear/src/agents.py
@@ -1,5 +1,5 @@
 import gymnasium as gym
-#import cvxpy as cp
+import cvxpy as cp
 import numpy as np
 import torch
 from torch.nn.functional import softmax
@@ -34,7 +34,7 @@ class SoftmaxPolicy:
         print(self.param.shape)
         X = torch.tensor(np.matmul(Phi_s, self.param))
         action_probs = softmax(X, dim=0)
-        return action_probs
+        return action_probs.numpy()
 
 
 
@@ -57,6 +57,7 @@ class Linear_Agent:
         self.eta = eta
         self.n_actions = n_actions
         self.reg_param = reg_param
+        self.embed_dim = embed_dim
     
     def act(self, state):
         action = self.policy.get_action(state)
@@ -65,7 +66,7 @@ class Linear_Agent:
     def get_cov(self, normalized=False):
         cov = self.reg_param*np.eye(self.embed_dim)
         for t in self.data:
-            phi = self.feature_map.compute_feature_vector(t['state'], t['action'])
+            phi = self.feature_map(t['state'], t['action'])
             cov = cov + np.outer(phi, phi)
         if normalized:
             n = len(self.buffer)
@@ -73,18 +74,111 @@ class Linear_Agent:
         return cov
 
 
-    def step():
+    def step(self):
         raise NotImplementedError
     
-    def train():
+    def train(self):
         raise NotImplementedError
 
 
 
 class OARAC_Agent(Linear_Agent):
 
-    def __init__(self, eta=0.01, beta=5, bw=20):
+    def __init__(self, 
+                 data: Replay_Buffer,
+                 env: gym.Env, 
+                 feature_map: Feature_Map, 
+                 n_actions=5, 
+                 eta=0.01,
+                 embed_dim=10,
+                 reg_param=1,
+                 beta=10,
+                 bw=10
+                 ):
+        
+        super().__init__(data,
+                         env,
+                         feature_map,
+                         n_actions=n_actions,
+                         eta=eta,
+                         embed_dim=embed_dim,
+                         reg_param=reg_param)
+        self.beta = beta
+        self.bw = bw
+
+    def get_problem_params(self):
+        """
+        Get the matrices for the critic optimization problem:
+            min v v^t e_1
+            st Av = R
+            v^tB_1v <= 1
+            v^t B_2v <= B_w
+            v^t B_3v <= beta
+
+        Returns:
+            tuple: matrices A in R^{d x 2d+1}
+            B_1 in R^{2d+1 x 2d+1}
+            B_2 in R^{2d+1 x 2d+1}
+            B_3 in R^{2d+1 x 2d+1}
+            R in R^{d}
+        """
+        d = self.embed_dim
+        A1 = np.zeros((d, d))
+        A2 = np.eye(d, d)
+        A3 = np.zeros((1, d))
+        R = np.zeros((1, d))
+
+        for t in self.data:
+            state = t['state']
+            action = t['action']
+            next_state = t['next_state']
+            reward = t['reward']
+
+            curr_feature = self.feature_map(state, action)
+            Phi_prime = self.feature_map.get_all_action_matrix(next_state)
+            action_probs = self.policy.get_action_probs(next_state)
+            next_feature = Phi_prime.T @ action_probs
+
+            A1 = A1 + curr_feature.reshape(-1, 1)
+            A3 = A3 + np.outer(curr_feature, next_feature)
+            R = R + reward*curr_feature.reshape(-1, 1)
+
+        Lambda = self.get_cov()
+        Lambda_inv = np.linalg.inv(Lambda)
+        A1 = Lambda_inv@A1
+        A3 = Lambda_inv@A3
+        A3 = np.eye(d, d) - A3
+        R = Lambda_inv@R
+
+        A = np.hstack((A1, A2, A3))
+
+        B_1 = np.zeros((2*d + 1, 2*d + 1))
+        B_1[0, 0] = 1
+
+        Z_d = np.zeros((d, d))
+
+        B_2 = np.block([
+            [np.zeros((1, d)), Z_d, Z_d],
+            [np.zeros((1, d)), Lambda, Z_d],
+            [np.zeros((1, d)), Z_d, Z_d]
+        ])
+
+        B_3 = np.block([
+            [np.zeros((1, d)), Z_d, Z_d],
+            [np.zeros((1, d)), Z_d, Z_d],
+            [np.zeros((1, d)), Z_d, np.eye(d, d)]
+        ])
+
+        return A, B_1, B_2, B_3, R
+
+
+
+
+
+    def step():
         pass
+
+
 
 
 
@@ -101,7 +195,11 @@ def test():
     policy.step(grad)
     action = policy.get_action(state)
     print(action)
-    print(policy.get_action_probs(state))
+    action_probs = policy.get_action_probs(state)
+    print(action_probs.reshape(-1, 1))
+    features = feature_map.get_all_action_matrix(state)
+    print(features)
+    print(features.T @ action_probs)
 
 
 if __name__ == '__main__':
